882fc2e6f8232d03c5d7ff21cc031b801d62a0fe
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-02-12 13:43:41 +0500

    hero component initiated

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -3,15 +3,29 @@ import PropTypes from 'prop-types'
 
 function Hero(props) {
   return (
-    <div>
-      <div>
-        <h1 className=''>M. Hammad Hassan</h1>
-        <p>Full Stack Developer (Backend Heavy)</p>
+
+    <div class="flex flex-wrap justify-between mt-10"> 
+      <div class="flex flex-col justify-evenly flex-grow basis-full md:basis-1/2 p-4 rounded-lg mr-4 ">
+        <div>
+
+          {/* MSG: for more highlighting options https://www.coding-dude.com/wp/css/highlight-text-css/ */}
+          <h1 class="font-bold text-4xl text-gray-700"> <span class="circle-sketch-highlight">M. Hammad Hassan </span> 
+
+          </h1>
+          <p class="mt-5 text-1xl text-gray-500">Full Stack Developer (Backend Heavy)</p>
+          <p className='flex flex-row flex-wrap gap-2'>
+            <a class="mt-5 text-1xl text-gray-500 cursor-pointer hover:text-lime-500">Linkedin </a>
+            <a class="mt-5 text-1xl text-gray-500 cursor-pointer hover:text-lime-500">Github </a>
+            <a class="mt-5 text-1xl text-gray-500 cursor-pointer hover:text-lime-500">Medium </a>
+            <a class="mt-5 text-1xl text-gray-500 cursor-pointer hover:text-lime-500">Hashnode </a>
+          </p>
+        </div>
       </div>
-      <div>
-        <img src="https://dummyimage.com/600x600/000/fff" class='rounded-5'/>
+      <div class="w-64 h-64 rounded-lg shadow ">
+        <img src="https://dummyimage.com/250x250/cacaca/fff" alt="Placeholder image" class="w-64 object-cover rounded-md  hover:shadow-xl transition duration-500 "/>
       </div>
     </div>
+
   )
 }
 
