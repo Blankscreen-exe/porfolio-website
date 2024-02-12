2372db49238116857c08313d71822cd72be1f315
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-02-12 13:57:39 +0500

    added hero styles and m-queries

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -9,16 +9,16 @@ function Hero(props) {
         <div>
 
           {/* MSG: for more highlighting options https://www.coding-dude.com/wp/css/highlight-text-css/ */}
-          <h1 class="font-bold text-4xl text-gray-700"> <span class="circle-sketch-highlight">M. Hammad Hassan </span> 
+          <h1 class="font-bold text-4xl text-gray-700"> <span class="circle-sketch-highlight md:before:w-sm">M. Hammad Hassan </span> 
 
           </h1>
           <p class="mt-5 text-1xl text-gray-500">Full Stack Developer (Backend Heavy)</p>
-          <p className='flex flex-row flex-wrap gap-2'>
-            <a class="mt-5 text-1xl text-gray-500 cursor-pointer hover:text-lime-500">Linkedin </a>
-            <a class="mt-5 text-1xl text-gray-500 cursor-pointer hover:text-lime-500">Github </a>
-            <a class="mt-5 text-1xl text-gray-500 cursor-pointer hover:text-lime-500">Medium </a>
-            <a class="mt-5 text-1xl text-gray-500 cursor-pointer hover:text-lime-500">Hashnode </a>
-          </p>
+          <div className='flex flex-row flex-wrap gap-2 mt-5'>
+            <a class="text-1xl text-gray-500 cursor-pointer hover:text-lime-500">Linkedin </a>
+            <a class=" text-1xl text-gray-500 cursor-pointer hover:text-lime-500">Github </a>
+            <a class=" text-1xl text-gray-500 cursor-pointer hover:text-lime-500">Medium </a>
+            <a class="text-1xl text-gray-500 cursor-pointer hover:text-lime-500">Hashnode </a>
+          </div>
         </div>
       </div>
       <div class="w-64 h-64 rounded-lg shadow ">
