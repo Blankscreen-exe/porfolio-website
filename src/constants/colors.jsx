1eb6895af1e712d1bd6bd42a70bd580adc76cae9
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-02-13 18:59:27 +0500

    add color constants

---
+++ src/constants/colors.jsx
@@ -0,0 +1,13 @@
+export const ColorConstants = {
+    primary: "#3FB07C",
+    primaryDark: "#",
+    primaryLight: "#",
+    secondary: "#",
+    secondaryDark: "#",
+    secondaryLight: "#",
+    tertiary: "#",
+    tertiaryDark: "#",
+    tertiaryLight: "#",
+    compliment: "#",
+    contrast: "#"
+};
