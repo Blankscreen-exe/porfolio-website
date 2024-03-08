6cf20aa9c9310dd796d311181afd367c93abdced
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-03-08 18:26:37 +0500

    cleaned

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -6,7 +6,7 @@ function NavBar(props) {
   return (
     <div
     id="navbar"
-    className="navbar bg-base-100 bg-slate-10 rounded-md shadow-md m-auto mt-10 flex justify-between sticky opacity-90 backdrop-blur-xl bg-white z-50"
+    className="navbar bg-base-100 bg-slate-10 rounded-md shadow-md m-auto mt-10 flex justify-between sticky opacity-90 backdrop-blur-xl bg-white z-[100]"
   >
       <div className="navbar-start">
         <div className="dropdown">
@@ -62,12 +62,15 @@ function NavBar(props) {
         </div>
         <a className="hover:text-neutral-600 transition-colors text-xl mx-2">{appConstants.common.pageTitle}</a>
       </div>
+
+      {/* ----------------------------- */}
+
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1 gap-5">
-          <li><a class="border border-[#0694a2]">Home</a></li>
+          <li><a class="border border-slate-400 hover:border-[#0694a2]">Home</a></li>
           <li>
             <details>
-              <summary class="text-[#0694a2]"><a>About Me</a></summary>
+              <summary class="hover:text-[#0694a2]"><a>About Me</a></summary>
               <ul className="p-2">
               <li><a>Education</a></li>
               <li><a>Hobbies</a></li>
@@ -77,7 +80,7 @@ function NavBar(props) {
           </li>
           <li>
             <details>
-              <summary class="text-[#0694a2]"><a>Experience</a></summary>
+              <summary class="hover:text-[#0694a2]"><a>Experience</a></summary>
               <ul className="p-2">
                 <li><a>Work History</a></li>
                 <li><a>Awards</a></li>
@@ -108,72 +111,3 @@ NavBar.propTypes = {}
 
 export default NavBar
 
-
-{/* <ul
-tabIndex="0"
-className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
->
-<li><a>Home</a></li>
-<li>
-  <a>About Me</a>
-  <ul className="p-2">
-    <li><a>Education</a></li>
-    <li><a>Hobbies</a></li>
-    <li><a>Languages I Speak</a></li>
-  </ul>
-</li>
-<li>
-  <a>Experience</a>
-  <ul className="p-2">
-    <li><a>Work History</a></li>
-    <li><a>Awards</a></li>
-    <li><a>Certificates</a></li>
-    <li><a>Working On Next</a></li>
-  </ul>
-</li>
-<li>
-  <a>List</a>
-  <ul className="p-2">
-    <li><a>Useful Resources</a></li>
-    <li><a>Games</a></li>
-  </ul>
-</li>
-<li><a>Projects</a></li>
-<li><a>Blog</a></li>
-<li><a>Contact</a></li>
-</ul>
-</div>
-<a className="hover:text-neutral-600 transition-colors text-xl mx-2">{appConstants.common.pageTitle}</a>
-</div>
-<div className="navbar-center hidden lg:flex">
-<ul className="menu menu-horizontal px-1">
-<li><a>Home</a></li>
-<li>
-  <a>About Me</a>
-  <ul className="p-2">
-    <li><a>Education</a></li>
-    <li><a>Hobbies</a></li>
-    <li><a>Languages I Speak</a></li>
-  </ul>
-</li>
-<li>
-  <a>Experience</a>
-  <ul className="p-2">
-    <li><a>Work History</a></li>
-    <li><a>Awards</a></li>
-    <li><a>Certificates</a></li>
-    <li><a>Working On Next</a></li>
-  </ul>
-</li>
-<li>
-  <a>List</a>
-  <ul className="p-2">
-    <li><a>Useful Resources</a></li>
-    <li><a>Games</a></li>
-  </ul>
-</li>
-<li><a>Projects</a></li>
-<li><a>Blog</a></li>
-<li><a>Contact</a></li>
-</ul>
-</div> */}
