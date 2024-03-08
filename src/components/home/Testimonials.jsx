86cab9a6d56bb4b2f8473b42345fc8c179afef5f
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-03-08 16:27:53 +0500

    cleaned

--- src/components/home/Testimonials.jsx
+++ src/components/home/Testimonials.jsx
@@ -2,6 +2,9 @@ import React, {useState, useRef} from 'react'
 import PropTypes from 'prop-types'
 import { TECarousel, TECarouselItem } from 'tw-elements-react'
 
+// data
+import testimonialData from '../../data/testimonials.json';
+
 function Testimonials(props) {
 
     const [currentSlide, setCurrentSlide] = useState(0);
@@ -38,7 +41,7 @@ function Testimonials(props) {
 
     <TECarousel showControls  ride="carousel"
     prevBtnIcon={
-          <>
+          <div className='hidden md:inline'>
             <span className="inline-block text-black h-8 w-8 [&>svg]:h-8">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -57,10 +60,10 @@ function Testimonials(props) {
             <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
               Previous
             </span>
-          </>
+          </div>
         }
     nextBtnIcon={
-        <div>
+        <div className='hidden md:inline'>
         <span className="inline-block text-black h-8 w-8 [&>svg]:h-8">
             <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -81,32 +84,31 @@ function Testimonials(props) {
         </span>
         </div>
     }
-    // theme={{
-    //     indicator:
-    //     "mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-black bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none",
-    // }}
     >
         <div className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
-            {/* TODO: dummy array */}
-            { [1,2,3,4].map( item => {
+            { testimonialData.map( (item, ind) => {
                 return (
                 <TECarouselItem
-                    key={item}
-                    itemID={item}
+                    key={ind}
+                    itemID={ind+1} // itemID starts from 1
                     className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
                 >
-                    <div className="flex flex-col justify-items-center items-center h-[650px] md:h-[450px]">
+                    <div className="flex flex-col justify-items-center items-center h-[550px] md:h-[350px]">
                         <img
-                            src="https://dummyimage.com/250x250/a9a9a9/fff"
+                            src={item.avatarUrl}
                             className="w-32 h-32 rounded-full"
-                            alt="dummy image ${item}"
+                            alt={item.title}
                             />
-                        <h3 className='text-xl font-medium text-gray-600 mt-5'>Some Title {item}</h3>
-                        <h4 className='text-lg italic font-medium text-gray-400'>Some Designation {item}</h4>
-                        <p className='font-light text-gray-600 mt-5 w-[65%] text-center mb-8'>{"Lorem ipsum dolor sit amet consectetur, adipisicing elit.".repeat(item)}</p>
+                        <h3 className='text-xl font-medium text-gray-600 mt-5'>{item.title}</h3>
+                        <h4 className='text-lg italic font-medium text-gray-400'>{item.designation}</h4>
+                        <div className='flex flex-col justify-between h-full'>
+                          <p className='font-light text-gray-600 mt-5 md:w-[65%] w-full text-center mb-8'>{item.content}</p>
+                    <div className='text-sm text-slate-400 md:hidden inline w-full flex justify-center border-white/5 border-t-slate-300'> slide for more testimonials </div>
+                        </div>
                     </div>
                 </TECarouselItem>);
             })}
+
 
                 
           
