1224b3d5d487f0cd41036a9e9feb5c81c9c6af24
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-03-15 11:32:52 +0500

    modified

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,15 +1,46 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+// Data
+import awards from '../../data/awards.json'
+
+// Constant
+import svgList from '../../constants/svg'
+import appConstants from '../../constants/appConstants'
+
 // Components
 import Hero from './Hero'
 import Testimonials from './Testimonials'
+import TechStack from './TechStack'
+import HorizontalSlider from '../common/HorizontalSlider'
+import CtaCard from '../common/CtaCard'
+import BlockQuote from '../common/BlockQuote'
 
 function Home(props) {
   return (
     <>
         <Hero/>
+        <BlockQuote
+          text="Some witty comments here which are very very long"
+        />
         <Testimonials/>
+        <TechStack/>
+        <HorizontalSlider
+          data={awards}
+          title={"Honors & Awards"}
+          buttonText={<><span>View</span> {svgList.externalLink}</>}
+        />
+        <HorizontalSlider
+          data={awards}
+          title={"Certificates"}
+          buttonText={<><span>View</span> {svgList.externalLink}</>}
+        />
+        <HorizontalSlider
+          data={awards}
+          title={"Projects"}
+          buttonText={<><span>View</span> {svgList.externalLink}</>}
+        />
+        <CtaCard/>
     </>
   )
 }
