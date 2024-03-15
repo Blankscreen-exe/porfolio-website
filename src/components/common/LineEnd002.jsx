84537b5d0d2dd088247c0bb39ed92e7ed488308a
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-03-15 16:47:32 +0500

    add new svg

---
+++ src/components/common/LineEnd002.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+// Constants
+import svgList from '../../constants/svg'
+
+function LineEnd002(props) {
+  return (
+    <div className='w-1/2 mx-auto my-10 text-slate-500'>{svgList.lineEnd002}</div>
+  )
+}
+
+LineEnd002.propTypes = {}
+
+export default LineEnd002
