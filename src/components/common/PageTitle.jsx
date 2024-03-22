4ec9cffb1f0c2986ea6e56f4176bdb016758da09
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-03-23 02:16:21 +0500

    tweaked to centralized

--- src/components/common/PageTitle.jsx
+++ src/components/common/PageTitle.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 function PageTitle(props) {
     const {title} = props;
     return (
-        <div className='flex items-center justify-center my-20'>
+        <div className='flex items-center justify-center my-20 text-center'>
             <p className="text-slate-700 font- text-5xl font-bold">{title}</p>
         </div>
     )
