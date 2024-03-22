c006957e6900a2906dc978b8338871d8f4510503
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-03-23 00:07:55 +0500

    add education component

---
+++ src/components/about/Education.jsx
@@ -0,0 +1,128 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Education(props) {
+  return (
+    <div class="p-4 max-w-md mx-auto pt-20 flow-root">
+  <ul role="list" class="-mb-8">
+    
+    
+    <li>
+      <div class="relative pb-8">
+        <span class="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>
+        <div class="relative flex space-x-3">
+          <div>
+            <span class="h-8 w-8 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-white drop-shadow-md">
+              <svg class="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z" />
+              </svg>
+            </span>
+          </div>
+          <div class="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5 mx-6">
+            <div>
+              <p class="text-sm text-gray-500">packing at <a href="#" class="font-medium text-gray-900">France</a></p>
+            </div>
+            <div class="whitespace-nowrap text-right text-sm text-gray-500">
+              <time datetime="2020-09-20">Sep 20</time>
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+    
+    
+    <li>
+      <div class="relative pb-8">
+        <span class="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>
+        <div class="relative flex space-x-3">
+          <div>
+            <span class="h-8 w-8 rounded-full  flex items-center justify-center ring-8 ring-white drop-shadow-md">
+              <img class="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"
+                src="https://t3.ftcdn.net/jpg/04/14/78/90/360_F_414789044_6xD0f4z9YcHfQimfnighWoUCIqgEJ66G.jpg" />
+            
+            </span>
+          </div>
+          <div class="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
+            <div>
+              <p class="text-sm text-gray-500">with driver <a href="#" class="font-medium text-gray-900">on the way to the plane</a></p>
+            </div>
+            <div class="whitespace-nowrap text-right text-sm text-gray-500">
+              <time datetime="2020-09-22">Sep 22</time>
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="relative pb-8">
+        <span class="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>
+        <div class="relative flex space-x-3">
+          <div>
+            <span class="h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white drop-shadow-md">
+              <img class="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"
+                src="https://c8.alamy.com/comp/2RC5DTJ/taking-off-plane-logo-2RC5DTJ.jpg" clip-rule="evenodd" />
+            
+            </span>
+          </div>
+          <div class="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
+            <div>
+              <p class="text-sm text-gray-500">on plane ready to fly <a href="#" class="font-medium text-gray-900"> to london</a></p>
+            </div>
+            <div class="whitespace-nowrap text-right text-sm text-gray-500">
+              <time datetime="2020-09-28">Sep 28</time>
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="relative pb-8">
+        <span class="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>
+        <div class="relative flex space-x-3">
+          <div>
+            <span class="h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white drop-shadow-md">
+              <img class="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"
+                 src="https://res.cloudinary.com/teepublic/image/private/s--AwgOGWhQ--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_auto:good:420,w_630/v1615488250/production/designs/20135311_0.jpg" />
+              
+            </span>
+          </div>
+          <div class="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
+            <div>
+              <p class="text-sm text-gray-500">with driver <a href="#" class="font-medium text-gray-900">on the way to you</a></p>
+            </div>
+            <div class="whitespace-nowrap text-right text-sm text-gray-500">
+              <time datetime="2020-09-30">Sep 30</time>
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="relative pb-8">
+        <div class="relative flex space-x-3">
+          <div>
+            <span class="h-8 w-8 rounded-full bg-green-500 flex items-center justify-center ring-8 ring-white drop-shadow-md">
+              <svg class="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+              </svg>
+            </span>
+          </div>
+          <div class="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
+            <div>
+              <p class="text-sm text-gray-500">wait outside the door <a href="#" class="font-medium text-gray-900">the driver is close</a></p>
+            </div>
+            <div class="whitespace-nowrap text-right text-sm text-gray-500">
+              <time datetime="2020-10-04">Oct 4</time>
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+  </ul>
+</div>
+  )
+}
+
+Education.propTypes = {}
+
+export default Education
