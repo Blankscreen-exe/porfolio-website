258b4d269f978c0f59b76c3be8f400d3597a7654
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-03-23 02:40:02 +0500

    tweaked title margins

--- src/components/common/SectionHeading.jsx
+++ src/components/common/SectionHeading.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 function SectionHeading(props) {
   return (
     <div className='flex flex-row justify-center items-center'>
-        <h1 className='text-3xl font-bold text-slate-600 mb-10'>{props.title}</h1>
+        <h1 className='text-3xl font-bold text-slate-600 my-10'>{props.title}</h1>
     </div>
   )
 }
