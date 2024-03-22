b7209f36d006d3a90d7f5480f61b7866d1590c8b
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-03-23 00:07:43 +0500

    add language component

---
+++ src/components/about/Languages.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+// Constants
+import svgList from "../../constants/svg";
+import imgList from "../../constants/img";
+
+function Languages(props) {
+  return (
+    <div className='flex flex-row flex-wrap justify-around'>
+    <div className="p-6 w-[45%]">
+        <div className="relative overflow-hidden rounded-2xl bg-gray-900 px-6 pb-9 shadow-xl shadow-slate-400">
+            <img
+            className="absolute inset-0 h-full w-full object-cover brightness-20 saturate-100"
+            src={imgList.cta.bg}
+            alt=""
+            />
+
+            <figure className="relative isolate">
+            {/* MAAL Starts */}
+
+            <blockquote className="mt-6 text-xl font-semibold leading-8 text-white">
+                <p className='text-3xl'>English</p>
+            </blockquote>
+            <figcaption className="mt-6 text-sm leading-6 text-gray-300">
+                Reason Why I speak English ... Or even so ... why I speak at all ...
+            </figcaption>
+
+            {/* MAAL Ends */}
+            </figure>
+        </div>
+        </div>
+        
+        <div className="p-6  w-[45%]">
+        <div className="relative overflow-hidden rounded-2xl bg-gray-900 px-6 pb-9 shadow-xl shadow-slate-400">
+            <img
+            className="absolute inset-0 h-full w-full object-cover brightness-20 saturate-100"
+            src={imgList.cta.bg}
+            alt=""
+            />
+
+            <figure className="relative isolate">
+            {/* MAAL Starts */}
+
+            <blockquote className="mt-6 text-xl font-semibold leading-8 text-white">
+                <p className='text-3xl'>German</p>
+            </blockquote>
+            <figcaption className="mt-6 text-sm leading-6 text-gray-300">
+                Reason Why I speak German ... Or even so ... why I speak at all ...
+            </figcaption>
+
+            {/* MAAL Ends */}
+            </figure>
+        </div>
+    </div>
+  </div>
+  )
+}
+
+Languages.propTypes = {}
+
+export default Languages
