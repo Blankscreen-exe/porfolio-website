2d76ed558c7482ae4ef946974084672d9c4bf945
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-03-23 00:50:32 +0500

    add white spaces

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -12,29 +12,32 @@ import Hobbies from './Hobbies'
 function About(props) {
   return (
     <div>
-        {/* TODO: add title */}
+
         <PageTitle
         title={"Who Am I?"}
         />
+
         <BlockQuote
             text="Some <b>awesome</b> stuff about me. I'm great! Sore dakke do ..."
         />
+
         <SectionHeading
             title="Education"
         />
-        {/* TODO: show vertical timeline */}
         <Education/>
+
         <br/>
+
         <SectionHeading
             title="Languages"
         />
-        {/* TODO: show two cards side by side like CTA card */}
         <Languages/>
+
         <br/>
+
         <SectionHeading
             title="Hobbies"
         />
-        {/* TODO: show doughnut chart */}
         <Hobbies/>
     </div>
   )
