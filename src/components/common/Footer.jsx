327d8037f79cf8289df563f705a39d3b20625ed7
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-03-26 22:00:50 +0500

    tweaked footer for theme

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -16,9 +16,9 @@ function Footer(props) {
 
   return (
     <div className='mb-5'>
-        <footer className="footer p-10 bg-base-200 text-base-content rounded-t-md">
+        <footer className="footer p-10 bg-tertiary/50 text-base-content rounded-t-md">
           <nav>
-            <h6 className="footer-title font-bold font-sans text-slate-900 border-b-2 border-b-primary">Pages</h6> 
+            <h6 className="footer-title font-bold font-sans text-content border-b-2 border-b-primary">Pages</h6> 
             <NavLink to={appConstants.routes.home} className={navLinkClasses}>Home</NavLink >
             <NavLink to={appConstants.routes.aboutMe} className={navLinkClasses}>About Me</NavLink >
             <NavLink to={appConstants.routes.projects} className={navLinkClasses}>Projects</NavLink >
@@ -27,27 +27,27 @@ function Footer(props) {
             <NavLink to={appConstants.routes.contacts} className={navLinkClasses}>Contact</NavLink >
           </nav> 
           <nav>
-            <h6 className="footer-title font-bold font-sans text-slate-900 border-b-2 border-b-primary">Services</h6> 
+            <h6 className="footer-title font-bold font-sans text-content border-b-2 border-b-primary">Services</h6> 
             <NavLink to={appConstants.routes.servicesBackend} className={navLinkClasses}>Backend</NavLink >
             <NavLink to={appConstants.routes.servicesFrontend} className={navLinkClasses}>Frontend</NavLink >
             <NavLink to={appConstants.routes.servicesDataScience} className={navLinkClasses}>Data Science</NavLink >
             <NavLink to={appConstants.routes.servicesMentorship} className={navLinkClasses}>Mentorship</NavLink >
           </nav> 
           <nav>
-            <h6 className="footer-title font-bold font-sans text-slate-900 border-b-2 border-b-primary">Lists</h6> 
+            <h6 className="footer-title font-bold font-sans text-content border-b-2 border-b-primary">Lists</h6> 
             <NavLink to={appConstants.routes.listsBooks} className={navLinkClasses}>Books</NavLink >
             <NavLink to={appConstants.routes.listsGames} className={navLinkClasses}>Games</NavLink >
             <NavLink to={appConstants.routes.listsResources} className={navLinkClasses}>Dev Resources</NavLink >
           </nav>
 
         </footer> 
-        <footer className="footer px-10 py-4 border-t bg-base-200 text-base-content border-base-300 rounded-b-md">
+        <footer className="footer px-10 py-4 border-t bg-tertiary/60 text-base-content border-base-300 rounded-b-md">
           <aside className="items-center grid-flow-col">
             {svgLists.hashIcon}
-            <p><b>M.Hammad Hassan</b> <br/>Code is a language I can speak!</p>
+            <p><b className='text-primary'>M.Hammad Hassan</b> <br/>Code is a language I can speak!</p>
           </aside> 
-          <nav className="md:place-self-center md:justify-self-end">
-            <div className="grid grid-flow-col gap-4">
+          <nav className="md:place-self-center md:justify-self-end w-full">
+            <div className="w-full grid grid-flow-col gap-4">
               {/* TODO: icons to add Linkedin, Github, Discord, E business card*/}
               <NavLink to={""}  className={navLinkClasses}>{svgLists.socialIcons.twitter}</NavLink >
               <NavLink to={""}  className={navLinkClasses}>{svgLists.socialIcons.youtube}</NavLink >
