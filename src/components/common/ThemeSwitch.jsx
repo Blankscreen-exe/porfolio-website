4bc50ec33d67fb2d851193de3417243755b62444
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-04-01 01:45:37 +0500

    deliciously mutiliated

--- src/components/common/ThemeSwitch.jsx
+++ src/components/common/ThemeSwitch.jsx
@@ -1,53 +1,59 @@
-import React, { useEffect, useState } from 'react'
-import PropTypes from 'prop-types'
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 
 // reducer
-import { useDispatch, useSelector } from 'react-redux'
-import { toggleThemeAction } from '../../redux/reducers/colorThemeSlice'
+import { useDispatch, useSelector } from "react-redux";
+import { toggleThemeAction } from "../../redux/reducers/colorThemeSlice";
 
 // Constants
-import svgLists from '../../constants/svg'
+import svgLists from "../../constants/svg";
 
 function ThemeSwitch(props) {
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
-  
   const isDarkMode = useSelector((state) => {
-    return state.persistedReducer.colorTheme
-  })
+    return state.persistedReducer.colorTheme;
+  });
 
   const handleToggleTheme = () => {
-      dispatch(toggleThemeAction())
-      document.documentElement.setAttribute("data-theme", isDarkMode? "dark" :"light")
-      console.log("THEME SWITCHED TO ", isDarkMode? "--DARK--" :"--LIGHT--")
-    }   
-    
-    
+    dispatch(toggleThemeAction());
+    document.documentElement.setAttribute(
+      "data-theme",
+      isDarkMode ? "dark" : "light"
+    );
+    console.log("THEME SWITCHED TO ", isDarkMode ? "--DARK--" : "--LIGHT--");
+  };
+
+  window.addEventListener("load", (event) => {
+    console.log("DOC LOADED ");
+    document.documentElement.setAttribute(
+      "data-theme",
+      isDarkMode ? "dark" : "light"
+    );
+  });
+
   return (
-    <div className='h-fit px-2 py-1 flex items-center rounded-md hover:bg-bg2'>
-        <label className="flex flex-row gap-2">
-  
+    <div className="h-fit px-2 py-1 flex items-center rounded-md hover:bg-bg2">
+      <label className="flex flex-row gap-2" onClick={handleToggleTheme}>
         {/* this hidden checkbox controls the state */}
         {/* <input type="checkbox" className='hidden' onClick={handleToggleTheme}/> */}
-        
-        {isDarkMode
-        ? svgLists.themeButton.light 
-        : svgLists.themeButton.dark
-        }
+
+        {isDarkMode ? svgLists.themeButton.light : svgLists.themeButton.dark}
 
         {/* moon icon */}
         {/* {svgLists.themeButton.dark} */}
 
         {/* sun icon */}
         {/* {svgLists.themeButton.light} */}
-        
-        <span className='hidden sm:block md:block lg:block' onClick={handleToggleTheme}>{isDarkMode? "Light Mode": "Dark Mode"}</span>
-        </label>
 
+        <span className="hidden sm:block md:block lg:block">
+          {isDarkMode ? "Light Mode" : "Dark Mode"}
+        </span>
+      </label>
     </div>
-  )
+  );
 }
 
-ThemeSwitch.propTypes = {}
+ThemeSwitch.propTypes = {};
 
-export default ThemeSwitch
+export default ThemeSwitch;
