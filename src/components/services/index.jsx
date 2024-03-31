b58186cb8501f1a4b044ef6a47fbfdfb4585927b
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-03-31 17:25:27 +0500

    add FAQ

--- src/components/services/index.jsx
+++ src/components/services/index.jsx
@@ -4,13 +4,17 @@ import PropTypes from 'prop-types'
 // Components
 import ServicesTabs from './ServicesTabs'
 import PageTitle from '../common/PageTitle'
+import FAQ from './FAQ'
+import SectionHeading from '../common/SectionHeading'
 
 function Services(props) {
   return (
     <div>
         <PageTitle title="What Can I Do For You?" />
         <ServicesTabs/>
-    
+        <SectionHeading title="Services FAQ" />
+        <FAQ/>
+
     </div>
   )
 }
