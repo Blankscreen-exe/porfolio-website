2d89af9ae91811146390aa64a05eb3a22f888338
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-04-01 16:56:45 +0500

    applied redux dispatcher

--- src/components/common/TextInput.jsx
+++ src/components/common/TextInput.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
+
+// Redux
+import { useDispatch, useSelector } from 'react-redux'
+import { updateFormAction } from '../../redux/reducers/contactFormSlice'
 
 // Components
 import { TEInput } from "tw-elements-react";
 
 function Input(props) {
   const { id, label, isRequired } = props;
+
+  const formData = useSelector((state) => {
+    return state.persistedReducer.contactForm
+  })
+
+  const dispatch = useDispatch();
+
+  const handleChange = (event) => {
+    const updatedFullName = event.target.value;
+    dispatch(updateFormAction({ fullName: updatedFullName }));
+  };
+
   return (
     <div className="my-2 custom-tw-input-elem">
       {isRequired ? (
@@ -13,6 +29,8 @@ function Input(props) {
           type="text"
           id={id ? id : "customTextInput"}
           label={<span className="bg-bg1 px-2">{label}</span>}
+          onClick={handleChange}
+          value={formData.fullName}
           required
         ></TEInput>
       ) : (
@@ -20,6 +38,7 @@ function Input(props) {
           type="text"
           id={id ? id : "customTextInput"}
           label={<span className="bg-bg1 px-2">{label}</span>}
+          onClick={useDispatch(updateFormAction({fullName:formData.fullName}))}
         ></TEInput>
       )}
     </div>
