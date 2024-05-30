8abbd6fef5eef8910798ceaa1b318849767f6188
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-05-30 23:18:15 +0500

    conditionally render protip

--- src/components/common/Protip.jsx
+++ src/components/common/Protip.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 function Protip(props) {
 
   return (
-    <div className="mx-auto w-fit my-6">
+    <div className="mx-auto w-fit my-6 md:visible invisible text-center">
       <p>
         <b>Protip:</b> <span className="kbd">Shift</span>+
         <span className="kbd">Scroll Wheel</span> to scroll horizontally
