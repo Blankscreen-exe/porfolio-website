94d2c46c33aa0f4b05772e56899752f4ace2df4b
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-05-30 13:32:01 +0500

    modified education component

--- src/components/about/Education.jsx
+++ src/components/about/Education.jsx
@@ -1,128 +1,107 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+// data
+import educationData from "../../data/educationTimeLine.json";
+
+// Helpers
+import { capitalizeFirstLetter } from "../../helpers/common";
+
+// Components
+import { TECollapse } from "tw-elements-react";
 
 function Education(props) {
+  const [activeElement, setActiveElement] = useState(-1);
+
+  const handleClick = (value) => {
+    if (value === activeElement) {
+      setActiveElement(-1);
+    } else {
+      setActiveElement(value);
+    }
+  };
+
   return (
-    <div className="p-4 max-w-md mx-auto flow-root">
-  <ul role="list" className="-mb-8">
-    
-    
-    <li>
-      <div className="relative pb-8">
-        <span className="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>
-        <div className="relative flex space-x-3">
-          <div>
-            <span className="h-8 w-8 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-bg2 drop-shadow-lg shadow-shadow">
-              <svg className="h-5 w-5 text-content" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z" />
-              </svg>
-            </span>
-          </div>
-          <div className="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5 mx-6 pl-3">
-            <div>
-              <p className="text-sm text-content">packing at <a href="#" className="font-medium text-contentLink/70 underline hover:text-contentLinkHover">France</a></p>
-            </div>
-            <div className="whitespace-nowrap text-right text-sm text-content">
-              <time datetime="2020-09-20">Sep 20</time>
-            </div>
-          </div>
-        </div>
-      </div>
-    </li>
-    
-    
-    <li>
-      <div className="relative pb-8">
-        <span className="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>
-        <div className="relative flex space-x-3">
-          <div>
-            <span className="h-8 w-8 rounded-full  flex items-center justify-center ring-8 ring-bg2 drop-shadow-lg shadow-shadow">
-              <img className="h-5 w-5 text-content" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"
-                src="https://t3.ftcdn.net/jpg/04/14/78/90/360_F_414789044_6xD0f4z9YcHfQimfnighWoUCIqgEJ66G.jpg" />
-            
-            </span>
-          </div>
-          <div className="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5 pl-3">
-            <div>
-              <p className="text-sm text-content">with driver <a href="#" className="font-medium text-contentLink/70 underline hover:text-contentLinkHover">on the way to the plane</a></p>
-            </div>
-            <div className="whitespace-nowrap text-right text-sm text-content">
-              <time datetime="2020-09-22">Sep 22</time>
-            </div>
-          </div>
-        </div>
-      </div>
-    </li>
-    <li>
-      <div className="relative pb-8">
-        <span className="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>
-        <div className="relative flex space-x-3">
-          <div>
-            <span className="h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-bg2 drop-shadow-lg shadow-shadow">
-              <img className="h-5 w-5 text-content" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"
-                src="https://c8.alamy.com/comp/2RC5DTJ/taking-off-plane-logo-2RC5DTJ.jpg" clipRule="evenodd" />
-            
-            </span>
-          </div>
-          <div className="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5 pl-3">
-            <div>
-              <p className="text-sm text-content">on plane ready to fly <a href="#" className="font-medium text-contentLink/70 underline hover:text-contentLinkHover"> to london</a></p>
-            </div>
-            <div className="whitespace-nowrap text-right text-sm text-content">
-              <time datetime="2020-09-28">Sep 28</time>
-            </div>
-          </div>
-        </div>
-      </div>
-    </li>
-    <li>
-      <div className="relative pb-8">
-        <span className="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>
-        <div className="relative flex space-x-3">
-          <div>
-            <span className="h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-bg2 drop-shadow-lg shadow-shadow">
-              <img className="h-5 w-5 text-content" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"
-                 src="https://res.cloudinary.com/teepublic/image/private/s--AwgOGWhQ--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_auto:good:420,w_630/v1615488250/production/designs/20135311_0.jpg" />
-              
-            </span>
-          </div>
-          <div className="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5 pl-3">
-            <div>
-              <p className="text-sm text-content">with driver <a href="#" className="font-medium text-contentLink/70 underline hover:text-contentLinkHover">on the way to you</a></p>
-            </div>
-            <div className="whitespace-nowrap text-right text-sm text-content">
-              <time datetime="2020-09-30">Sep 30</time>
-            </div>
-          </div>
-        </div>
-      </div>
-    </li>
-    <li>
-      <div className="relative pb-8">
-        <div className="relative flex space-x-3">
-          <div>
-            <span className="h-8 w-8 rounded-full bg-green-500 flex items-center justify-center ring-8 ring-bg2 drop-shadow-lg shadow-shadow">
-              <svg className="h-5 w-5 text-content" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fillRule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clipRule="evenodd" />
-              </svg>
-            </span>
-          </div>
-          <div className="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5 pl-3">
-            <div>
-              <p className="text-sm text-content">wait outside the door <a href="#" className="font-medium text-contentLink/70 underline hover:text-contentLinkHover">the driver is close</a></p>
-            </div>
-            <div className="whitespace-nowrap text-right text-sm text-content">
-              <time datetime="2020-10-04">Oct 4</time>
-            </div>
-          </div>
-        </div>
-      </div>
-    </li>
-  </ul>
-</div>
-  )
+    <div className="p-4 w-[80%] md:w-[50%] mx-auto flow-root">
+      <ul role="list" className="-mb-8">
+        {educationData.map((item, ind) => {
+          return (
+            <li key={ind}>
+              <div className="relative pb-8">
+                {educationData.length - 1 !== ind && (
+                  <span
+                    className="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-200"
+                    aria-hidden="true"
+                  ></span>
+                )}
+                <div className="relative flex space-x-3">
+                  <div>
+                    <span className="h-8 w-8 rounded-full bg-primary flex items-center justify-center ring-8 ring-bg2 drop-shadow-lg shadow-shadow">
+                      <svg
+                        className="h-5 w-5 text-content"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                  <div className="flex min-w-0 flex-1 justify-between space-x-4  pl-3">
+                    <div>
+                      {/* ---------------------------------------------- */}
+
+                      <div className="my-auto">
+                        <h2 className="mb-0" id="headingOne">
+                          <button
+                            className={`${
+                              activeElement === ind
+                                ? `text-primary`
+                                : "text-content/70"
+                            }  relative flex w-full items-center rounded-none text-left text-sm md:text-lg font-bold transition outline-none focus:outline-none hover:outline-none outline-0 hover:outline-0 focus:outline-0 hover:border-none hover:border-0 border-0 hover:text-primary`}
+                            type="button"
+                            onClick={() => handleClick(ind)}
+                            aria-expanded="true"
+                            aria-controls="collapseOne"
+                          >
+                            {item.degreeTitle}
+                          </button>
+                        </h2>
+                        <TECollapse
+                          show={activeElement === ind}
+                          className="!mt-0 !rounded-b-none !shadow-none overflow-visible"
+                        >
+                          <div
+                            className="-px-5 md:px-5 py-4 text-xs md:text-sm w-[200%] sm:w-[100%] md:w-[100%]"
+                            dangerouslySetInnerHTML={{
+                              __html: item.description,
+                            }}
+                          ></div>
+                        </TECollapse>
+                      </div>
+
+                      {/* ---------------------------------------------- */}
+                    </div>
+                    <div className="whitespace-nowrap text-right text-xs md:text-sm text-content">
+                      <time datetime="2020-10-04">
+                        {item.startDate} - {item.endDate}
+                      </time>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
-Education.propTypes = {}
+Education.propTypes = {};
 
-export default Education
+export default Education;
