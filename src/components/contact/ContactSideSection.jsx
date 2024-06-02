00422772e878fac842744c19c82fcfa0f8d3f444
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-06-02 13:41:12 +0500

    finalized contact me text

--- src/components/contact/ContactSideSection.jsx
+++ src/components/contact/ContactSideSection.jsx
@@ -3,31 +3,34 @@ import PropTypes from "prop-types";
 
 // data
 import contactList from "../../data/contactAlt.json";
+import socialLinks from '../../data/socialLinks.json';
+
+// constants
+import svgList from '../../constants/svg'
 
 function ContactSideSection(props) {
   return (
     <div className="px-6 rounded-md mb-16 w-fit">
       <h1 className="text-3xl font-bold text-title my-3">Hello!</h1>
-      <p>You can try the following alternatives to contact me as well.</p>
+      <p>I'd love to hear from you! Feel free to drop me a line if you have any questions, project inquiries, or just want to chat.</p>
 
       {contactList.map((item, ind) => {
         return (
-          <a href={item.link} target="_blank" className="w-full">
+          <a href={socialLinks[item.icon]} target="_blank" className="w-full">
             <div
               key={ind}
               className="flex flex-row gap-4 items-center text-content hover:text-primary hover:cursor-pointer my-4"
             >
-              <img
-                src="https://dummyimage.com/400x400/878787/fff"
-                className="w-12 rounded-full"
-              />
+              <div className="w-12 rounded-full">
+                {svgList.socialIcons[item.icon]}
+              </div>
               <span className="font-bold">{item.name}</span>
             </div>
           </a>
         );
       })}
 
-      <p>Short note at the end of the section.</p>
+      <p>I'm always happy to connect!</p>
     </div>
   );
 }
