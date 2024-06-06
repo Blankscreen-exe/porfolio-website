1ae8aa55ff1b665160e97bb59ba4f52b4d50ee77
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-06-06 20:53:38 +0000

    fix(blog): card size

--- src/components/blog/BlogCard.jsx
+++ src/components/blog/BlogCard.jsx
@@ -13,13 +13,13 @@ function BlogCard(props) {
     <div>
         <a href={postLink}>
       <div className="mb-6 flex flex-wrap w-full">
-        <div className="mb-6 ml-auto w-full h-36 shrink-0 grow-0 basis-auto px-3 md:mb-0 md:w-3/12">
+        <div className="mb-6 ml-auto w-full md:w-full lg:w-52 p-4 shrink-0 grow-0 basis-auto px-3 md:mb-0">
           <div
             className="relative mb-6 overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
             data-te-ripple-init
             data-te-ripple-color="light"
           >
-            <img src={imgUrl} className="w-full h-40 object-cover"  alt="blog image" />
+            <img src={imgUrl} className="w-full h-full lg:w-52 lg:h-44 object-cover"  alt="blog image" />
             <a href={postLink}>
               <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100 bg-[hsla(0,0%,98.4%,.15)]"></div>
             </a>
