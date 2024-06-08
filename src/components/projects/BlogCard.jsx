db062df348a2d87b0b650d27ae137d98e4984245
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-06-08 23:56:36 +0500

    applied same styling as blog roll

--- src/components/projects/BlogCard.jsx
+++ src/components/projects/BlogCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 // helpers
-import { truncateText } from "../../helpers/common";
+import { capitalizeWords, truncateText } from "../../helpers/common";
 
 function BlogCard(props) {
   const { cardTitle, cardDescription, date, tags, imgUrl, projLink, githubLink } = props;
@@ -12,42 +12,24 @@ function BlogCard(props) {
   return (
     <div>
         <a href={projLink}>
-      <div className="mb-6 flex flex-wrap">
-        <div className="mb-6 ml-auto w-full shrink-0 grow-0 basis-auto px-3 md:mb-0 md:w-3/12">
+      <div className="mb-6 flex flex-col lg:flex-row w-full">
+        <div className="mb-6 ml-auto w-full md:w-full lg:w-52 xl:w-52 shrink-0 grow-0 basis-auto px-3 md:mb-0">
           <div
-            className="relative mb-6 overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
+            className="relative mb-6 overflow-hidden rounded-xl lg:rounded-lg xl:rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
             data-te-ripple-init
             data-te-ripple-color="light"
           >
-            <img src={imgUrl} className="w-full" alt="Louvre" />
-            <a href="#!">
+            <img src={imgUrl} className="w-full h-full lg:w-full xl:w-52 lg:h-44 xl:h-44 object-cover"  alt="blog image" />
+            <a href={projLink}>
               <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100 bg-[hsla(0,0%,98.4%,.15)]"></div>
             </a>
           </div>
         </div>
 
-        <div className="mb-6 mr-auto w-full shrink-0 grow-0 basis-auto px-3 md:mb-0 md:w-9/12 xl:w-7/12">
-          <h5 className="mb-1 text-lg font-bold">{cardTitle}</h5>
-          {/* REVIEW: enable svg ? */}
-          {/* <div className="mb-3 flex items-center justify-center text-sm font-medium text-danger dark:text-danger-500 md:justify-start">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="currentColor"
-              className="mr-2 h-5 w-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12.75 3.03v.568c0 .334.148.65.405.864l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 01-1.161.886l-.143.048a1.107 1.107 0 00-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 01-1.652.928l-.679-.906a1.125 1.125 0 00-1.906.172L4.5 15.75l-.612.153M12.75 3.031a9 9 0 00-8.862 12.872M12.75 3.031a9 9 0 016.69 14.036m0 0l-.177-.529A2.25 2.25 0 0017.128 15H16.5l-.324-.324a1.453 1.453 0 00-2.328.377l-.036.073a1.586 1.586 0 01-.982.816l-.99.282c-.55.157-.894.702-.8 1.267l.073.438c.08.474.49.821.97.821.846 0 1.598.542 1.865 1.345l.215.643m5.276-3.67a9.012 9.012 0 01-5.276 3.67m0 0a9 9 0 01-10.275-4.835M15.75 9c0 .896-.393 1.7-1.016 2.25"
-              />
-            </svg>
-            Travels
-          </div> */}
+        <div className="mb-6 mr-auto w-full shrink-0 grow-0 basis-auto px-3 md:mb-0 lg:w-9/12 xl:w-7/12">
+          <h5 className="mb-1 text-2xl lg:text-lg xl:text-lg font-bold text-content hover:text-primary">{cardTitle}</h5>
 
-          <div className="my-1 text-content/60">
+          <div className="my-1 text-content/60 text-2xl lg:text-lg xl:text-lg">
             <small>
               Published <u>{date}</u>
             </small>
@@ -57,13 +39,13 @@ function BlogCard(props) {
             {tags.map((item) => {
               console.log(item);
               return (
-                <div className="badge text-content hover:bg-primary/60 hover:text-white hover:cursor-default font-bold rounded-md border border-primary/30 px-2 py-0.5">
-                  {item}
+                <div className="badge flex flex-wrap text-content hover:bg-contrast/70 hover:text-white hover:cursor-default font-bold rounded-md border border-contrast/50 px-2 py-0.5 text-md lg:text-sm xl:text-sm">
+                  {capitalizeWords(item)}
                 </div>
               );
             })}
           </div>
-          <p className="mt-2 text-content ">
+          <p className="mt-2 text-content text-lg lg:text-sm ">
             {truncateText(cardDescription, descriptionSize)}
           </p>
         </div>
