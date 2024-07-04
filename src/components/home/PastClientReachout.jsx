9018d3166be16003ca80d9db52a40360671dc041
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-07-05 01:22:57 +0500

    changed para

--- src/components/home/PastClientReachout.jsx
+++ src/components/home/PastClientReachout.jsx
@@ -9,7 +9,7 @@ function PastClientReachout(props) {
   return (
     <div>
         <div className='mb-12'>
-            <p className="mx-auto w-[100%] font-normal text-center text-content" >Please consider leaving a testimonial on <a href={socialLinks.upwork} target='_blank' className='text-contentLinkHover underline'> Upwork</a> or <a href={socialLinks.upwork} target='_blank' className='text-contentLinkHover underline'>Linkedin</a>. That would mean a whole world to me!</p>
+            <p className="mx-auto w-[100%] font-normal text-center text-content" >If you are one of my students or a client with which I have worked before or even someone with whom I have had the pleasure of working alongside with, then please consider leaving a recommendation on my <a href={socialLinks.upwork} target='_blank' className='text-contentLinkHover underline'>Upwork Profile</a>. That would mean a whole world to me!</p>
         </div>
     </div>
   )
